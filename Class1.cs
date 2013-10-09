405c6f7f614796ce24b7e24c645ae5d26ad07ece
TAZekveld <terence@vvconsult.co.za>
2013-10-09 07:44:57 +0200

    StringCalculator exercises 1,2,3,4,6

---
+++ Class1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace class1
+{
+    public class NegativeException : ApplicationException
+    {
+
+        public NegativeException(string arg1)
+        { 
+        }
+    }
+
+    [TestFixture]
+    public class TestCalculator
+    {
+        Calculator Calc;
+
+        [SetUp]
+        public void Setup()
+        {
+            Calc = new Calculator();
+        }
+
+        [Test]
+        public void EmptyString_ReturnZero()
+        {
+            int result = Calc.Add("");
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        [TestCase("5", 5)]
+        [TestCase("25", 25)]
+        public void GivenSingleNr_ReturnsTheNr(string input, int Expected)
+        {
+            int result = Calc.Add(input);
+            Assert.That(result, Is.EqualTo(Expected));
+        }
+
+        [Test]
+        [TestCase("5,7", 12)]
+        [TestCase("1,2", 3)]
+        [TestCase("9,1017", 9)]
+        public void Given2Nrs_ReturnsSum(string input, int Expected)
+        {
+            //int Result = Calc.Add("5,7");
+            //Assert.That(Result, Is.EqualTo(12));
+            int Result = Calc.Add(input);
+            Assert.That(Result, Is.EqualTo(Expected));
+        }
+
+        [Test]
+        [TestCase("5,9,3", 17)]
+        [TestCase("", 0)]
+        [TestCase("5", 5)]
+        [TestCase("5\n9,3,13,123,1", 154)]
+        [TestCase("5\n9\n3\n13\n123\n1", 154)]
+        public void GivenManyNrs_ReturnsSum(string input, int Expected)
+        {
+            int Result = Calc.Add(input);
+            Assert.That(Result, Is.EqualTo(Expected));
+        }
+
+        [Test]
+        public void GivenDelimeter_ReturnSum()
+        {
+            int result = Calc.Add("//;\n1;2");
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        [TestCase("5", 5)]
+        [TestCase("2005", 0)]
+        [TestCase("5,2005", 5)]
+        [TestCase("5,1000", 1005)]
+        [TestCase("5,1001", 5)]
+        public void GivenNrsGT1000_IgnoreThoseGT1000(string Input, int Expected)
+        {
+            int Result = Calc.Add(Input);
+            Assert.That(Result, Is.EqualTo(Expected));
+        }
+        //[Test]
+        //public void GivenNegative_ReturnErrorAndNegNrs()
+        //{
+        //    int result = Calc.Add("-1,-2");
+        //    Assert.Throws(typeof(NegativeException), '');
+
+        //}
+    }
+
+    public class Calculator
+    {
+
+        public int Add(string numbers)
+        {
+            string delimeter = ",";
+
+            if (string.IsNullOrEmpty(numbers))
+                return 0;
+
+            //// test if first 2 characters are // - indicating that the delimeter is contained in the sent string
+            if (numbers.Length > 2)
+            {
+                if (numbers.Substring(0, 2) == "//")
+                {
+                    numbers = numbers.Substring(2, numbers.Length - 2);
+                    string[] parts1 = numbers.Split('\n');
+                    delimeter = parts1[0];
+                    numbers = numbers.Substring(2, numbers.Length - 2);
+                }
+            }
+
+
+            //if (numbers.Contains(","))
+            //{
+            //    var Res = numbers.Split(',');
+            //    return int.Parse(Res[0]) + int.Parse(Res[1]);
+            //}
+
+            if (numbers.Contains(",") || numbers.Contains("\n") || numbers.Contains(delimeter))
+            {
+                int result = 0;
+                
+                char[] delimiters = new char[] { ',', '\n', delimeter[0] };
+                string[] parts = numbers.Split(delimiters);//, StringSplitOptions.RemoveEmptyEntries);
+
+                for (int i = 0; i < parts.Length; i++)
+                {
+                    //if (int.Parse(parts[1]) < 0)
+                    //{
+                    //    throw NegativeException(parts[1]);
+                    //    return 0;
+                    //}
+                    if (int.Parse(parts[i]) <= 1000)
+                      result += int.Parse(parts[i]);
+                }
+                //var Res = numbers.Split(',');
+                //return int.Parse(Res[0]) + int.Parse(Res[1]);
+                return result;
+            }
+
+            //if (int.Parse(numbers) < 0)
+            //{
+            //    throw NegativeException(numbers);
+            //    return 0;
+            //}
+
+            if (int.Parse(numbers) <= 1000)
+                return int.Parse(numbers);
+            else
+                return 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringKateTAZ
+{
+    class Program
+    {
+        public class NegativeException : ApplicationException
+        {
+            
+            public NegativeException(string arg1)
+            {}
+        }
+
+        static void Main(string[] args)
+        {
+            // just some tests
+
+            //string delimeter = ",";
+            ////string numbers = "5\n9\n3\n13\n123\n1";
+            //string numbers = "//;\n1;2";
+            //if (numbers.Substring(0,2) == "//")
+            //{
+            //    numbers = numbers.Substring(2, numbers.Length - 2);
+            //    string[] parts = numbers.Split('\n');
+            //    delimeter = parts[0];
+            //    numbers = numbers.Substring(2, numbers.Length - 2);
+            //}
+
+                //throw new NegativeException("negative numbers not allowed -5");
+
+                    //System.ArgumentException("Numbers cannot be negative");//, "original");   
+
+
+            //numbers = numbers.Substring(2, numbers.Length - 2);
+            //Console.WriteLine(numbers);
+            //var delimeter = numbers.Split('\n');
+            //numbers = numbers.Substring(2, numbers.Length - 2);
+            //Console.WriteLine(delimeter + "  " + numbers);
+            //string s = "1,2,3,4";
+            //int nSeparators = 0;
+            ////char[] delimiters = new char[] { '\r', '\n' };
+            //char[] delimiters = new char[] { ','};
+            //string[] parts = s.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            //for (int i = 0; i < parts.Length; i++)
+            //{
+            //    Console.WriteLine(parts[i]);
+            //}
+
+
+        }
+
+    }
+}
